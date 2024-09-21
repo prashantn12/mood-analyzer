0e98a46c9eda4e48a71d0008808b225496eaefe1
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-21 12:29:01 +0530

    Added: To make the Test case pass analyseMood method need to check for Sad else return HAPPY

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 class MoodAnalyzer {
 
-
     private String message;
+
+
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
     public String analyzeMood() {
-        // Check if the message contains the word "Sad"
         if (message.contains("Sad")) {
             return "SAD";
         } else {
@@ -19,8 +19,9 @@ class MoodAnalyzer {
     }
 
     public static void main(String[] args) {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
-        System.out.println(moodAnalyzer.analyzeMood());  // Output: SAD
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any Mood");
+        System.out.println(moodAnalyzer.analyzeMood());  // Output: HAPPY
     }
 }
+
 
--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
-    
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSAD() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+    public void givenMessage_WhenNotSad_ShouldReturnHAPPY() {
 
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any Mood");
+        
         String mood = moodAnalyzer.analyzeMood();
 
-        Assert.assertEquals("SAD", mood);
+        Assert.assertEquals("HAPPY", mood);
     }
 }
